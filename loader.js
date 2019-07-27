7a29e45e0cf2f89adcabb61ea10d64288b51b7cc
HeadMad <kov.ps@ya.ru>
2019-07-27 16:57:55 +0800

    new working version

--- loader.js
+++ loader.js
@@ -5,26 +5,8 @@ const compiler = require('vue-template-compiler')
 const { requirePeer, camelize, capitalize, hyphenate } = require('./util')
 const instalatorPath = require.resolve('./lib/runtimeInstall.js')
 
-function getMatches(items, matches) {
-  let imports = []
-  items.forEach(({tag, node, ...params}) => {
-    for (let matcher of matches) {
-      let match = matcher(tag, params, node)
-      
-      if (!match)
-      continue
 
-      if (Array.isArray(match))
-      imports = imports.concat(match)
-
-      else
-      imports.push(match)
-    }
-  })
-
-  return imports
-}
-
+/* HELPERS START */
 function _makeInstallsArray(imports) {
   let installs = imports.reduce((inst, imp) => {
     if (!imp.install || !imp.name) return inst
@@ -61,6 +43,28 @@ function _makeCodesArray(imports) {
 
   return codes
 }
+/* HELPERS END */
+
+
+function getMatches (items, matches) {
+  const imports = []
+
+  items.forEach(({tag, node, ...params}) => {
+    for (let matcher of matches) {
+
+      let rootPath = this.rootContext
+      let filePath = this.resourcePath.substring(this.rootContext.length + 1)
+
+      let match = matcher(tag, params, node, {rootPath, filePath})
+      if (!match) continue
+
+      imports.push(match)
+    }
+    
+  });
+
+  return imports
+}
 
 function install (content, imports) {
   if (!imports.length) return content
@@ -93,7 +97,6 @@ function install (content, imports) {
   return content
 }
 
-
 module.exports = async function (content, sourceMap) {
   this.async()
   this.cacheable()
@@ -103,72 +106,75 @@ module.exports = async function (content, sourceMap) {
     ...loaderUtils.getOptions(this)
   }
 
-  options.match = !Array.isArray(options.match) ? [options.match] : options.match
+  if (!Array.isArray(options.match)) options.match = [options.match]
 
-  if (!this.resourceQuery) {
-    const readFile = path => new Promise((resolve, reject) => {
-      this.fs.readFile(path, function (err, data) {
-        if (err) reject(err)
-        else resolve(data)
-      })
+  if (this.resourceQuery)
+    return this.callback(null, content, sourceMap)
+    
+  const readFile = path => new Promise((resolve, reject) => {
+    this.fs.readFile(path, function (err, data) {
+      if (err) reject(err)
+      else resolve(data)
     })
+  })
   
-    this.addDependency(this.resourcePath)
+  this.addDependency(this.resourcePath)
   
-    const installNodes = []
+  const file = (await readFile(this.resourcePath)).toString('utf8')
+  const component = compiler.parseComponent(file)
   
-    const file = (await readFile(this.resourcePath)).toString('utf8')
-    const component = compiler.parseComponent(file)
-    if (component.template) {
-      if (component.template.src) {
-        const externalFile = path.resolve(path.dirname(this.resourcePath), component.template.src);
-        const externalContent = (await readFile(externalFile)).toString('utf8')
-        component.template.content = externalContent
-      }
-      if (component.template.lang === 'pug') {
-        const pug = requirePeer('pug')
-        try {
-          component.template.content = pug.render(component.template.content)
-        } catch (err) {/* Ignore compilation errors, they'll be picked up by other loaders */}
-      }
-  
-      compiler.compile(component.template.content, {
-        modules: [{
-          postTransformNode: node => {
-            let tag, props, directives
-  
-            // get original tag and modifications
-            tag = {
-              rawTag: node.tag,
-              kebabTag: hyphenate(node.tag),
-              camelTag: capitalize(camelize(node.tag))
-            }
-  
-            if (node.directives) {
-              directives = node.directives
-            }
-  
-            // get props of tag
-            if (node.attrs) {
-              props = node.attrs.map(attr => {
-                let name = attr.name
-                let isBinded = attr.dynamic !== undefined
-                let value = !isBinded ? node.attrsMap[name] : node.attrsMap[':' + name] ? node.attrsMap[':' + name] : node.attrsMap['v-bind:' + name]
-                return {name, value, isBinded}
-              })
-            }
-  
-            installNodes.push({tag, props: props, directives: directives, node})
-          }
-        }]
-      })
-    }
-  
-    if (options.match.length) {
-      let imports = getMatches(installNodes, options.match)
-      content = install(content, imports)
-    }
+  if (!component.template)
+    return this.callback(null, content, sourceMap)
+
+  if (component.template.src) {
+    const externalFile = path.resolve(path.dirname(this.resourcePath), component.template.src);
+    const externalContent = (await readFile(externalFile)).toString('utf8')
+    component.template.content = externalContent
   }
-   
+  if (component.template.lang === 'pug') {
+    const pug = requirePeer('pug')
+    try {
+      component.template.content = pug.render(component.template.content)
+    } catch (err) {/* Ignore compilation errors, they'll be picked up by other loaders */}
+  }
+
+  const installNodes = []
+  compiler.compile(component.template.content, {
+    modules: [{
+      postTransformNode: node => {
+        let tag, props, directives, className
+
+        // get original tag and modifications
+        tag = {
+          rawTag: node.tag,
+          kebabTag: hyphenate(node.tag),
+          camelTag: capitalize(camelize(node.tag))
+        }
+
+        if (node.directives) {
+          directives = node.directives
+        }
+
+        // get props of tag
+        if (node.attrs) {
+          props = node.attrs.map(attr => {
+            let name = attr.name
+            let isBinded = attr.dynamic !== undefined
+            let value = !isBinded ? node.attrsMap[name] : node.attrsMap[':' + name] ? node.attrsMap[':' + name] : node.attrsMap['v-bind:' + name]
+            return {name, value, isBinded}
+          })
+        }
+
+        if (node.staticClass) {
+          className = node.attrsMap.class.trim().split(' ')
+        }
+
+        installNodes.push({tag, className, props, directives, node})
+      }
+    }]
+  })
+
+  content = install(content, getMatches.call(this, installNodes, options.match))
+  
   this.callback(null, content, sourceMap)
 }
